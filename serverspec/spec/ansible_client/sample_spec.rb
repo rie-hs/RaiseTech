b7de9f1431c28a31781e0db4f769f550a27c4d09
Rie-hs <143470167+rie-hs@users.noreply.github.com>
2024-02-15 15:41:11 +0900

    serverspecの実行

--- serverspec/spec/ansible_client/sample_spec.rb
+++ serverspec/spec/ansible_client/sample_spec.rb
@@ -16,14 +16,3 @@ end
 describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
   its(:stdout) { should match /^200$/ }
 end
-
-# Railsが指定したバージョンでインストールされているか確認 
-describe command('rails --version') do
-  its(:stdout) { should match /Rails (\d+\.\d+\.\d+)/ }
-  its(:stdout) { should match /Rails 7\.0\.4/ }
-end
-
-# MySQLのサービスが正常に稼働している
-describe service('mysqld') do
-  it { should be_enabled   }
-end
